d7d3fbcb29d329b325c824f5135349110b1e0272
Poleswar23 <145527288+Poleswar23@users.noreply.github.com>
2024-03-03 00:03:50 +0530

    Update streamlit_app.py
    check4

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on the Smoothie will be:', name_on_order)
 cnx=st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name','search_on'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
